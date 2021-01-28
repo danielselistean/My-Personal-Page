4a21dee3bcbbd39f8dad6597988b05077fe3dc4f
Daniel <daniel.selistean@yahoo.com>
2021-01-28 13:47:48 +0200

    resume.io

--- src/Resume.js
+++ src/Resume.js
@@ -10,7 +10,7 @@ class Resume {
                                         <h1>Curriculum Vitae</h1>
                                     </div>
                                     <div class = 'resume-link'>
-                                        <a href = "https://resume.io/r/ue2ortOmE">My '.Io' Resume</a>
+                                        <a href = "https://resume.io/r/ue2ortOmE">Resume.io</a>
                                     </div>
                                     <div class = 'resume-text'>
                                         <h2>Vasile-Daniel Selistean</h2>
